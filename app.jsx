9afb79fdcc67fa69a88780a357d5fad15ed15da1
Pamolaan113 <148740319+Pamolaan113@users.noreply.github.com>
2023-11-05 06:41:17 +0700

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet,Text,View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const App =() =>{
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
+
+const Styles = StyleSheet.create({})
+
+  
